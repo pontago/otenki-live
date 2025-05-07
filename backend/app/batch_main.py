5365cdbd21c2a76558137d1510204e9a0172b0c2
pontago <pontagoo@gmail.com>
2025-05-07 20:49:26 +0900

    docs: DB設計追加。 style: Ruff設定追加。

---
+++ backend/app/batch_main.py
@@ -0,0 +1,49 @@
+import boto3
+import typer
+from boto3.dynamodb.conditions import Key
+
+
+def main():
+    dynamodb = boto3.resource(
+        "dynamodb",
+        region_name="ap-northeast-1",
+        endpoint_url="http://localhost:8000",
+        aws_access_key_id="dummy",
+        aws_secret_access_key="dummy",
+    )
+    # dynamodb.create_table(
+    #     TableName="WeatherForecast",
+    #     KeySchema=[{"AttributeName": "area", "KeyType": "HASH"}, {"AttributeName": "date", "KeyType": "RANGE"}],
+    #     AttributeDefinitions=[
+    #         {"AttributeName": "area", "AttributeType": "S"},
+    #         {"AttributeName": "date", "AttributeType": "S"},
+    #     ],
+    #     ProvisionedThroughput={"ReadCapacityUnits": 5, "WriteCapacityUnits": 5},
+    # )
+
+    table = dynamodb.Table("WeatherForecast")
+
+    table.put_item(
+        Item={
+            "area": "101",
+            "date": "2025-03-10#2025-04-30T12:00:00",
+        },
+    )
+    table.put_item(
+        Item={
+            "area": "101",
+            "date": "2025-03-11#2025-04-30T13:00:00",
+        },
+    )
+
+    response = table.query(
+        KeyConditionExpression=Key("area").eq("101"),
+        ScanIndexForward=False,
+        Limit=1,
+    )
+    items = response.get("Items", [])
+    print(items)
+
+
+if __name__ == "__main__":
+    typer.run(main)
